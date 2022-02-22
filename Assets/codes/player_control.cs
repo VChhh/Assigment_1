8a504a1a0ba5b6252303cd7857c43a7ee5a88d0e
VChhh <ql2015@nyu.edu>
2022-02-22 03:52:00 -0500

    move and jump

--- Assets/codes/player_control.cs
+++ Assets/codes/player_control.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class player_control : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int speed_const = 10;
+    public int jump_const = 500;
+    Rigidbody2D _rb;
+    public LayerMask ground;
+    public Transform feet;
+    float ground_check_dist = .2f;
+    bool grounded = false;
+    
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void FixedUpdate() {
+        grounded = Physics2D.OverlapCircle(feet.position, ground_check_dist, ground); // check if grounded
+    }
+
     void Update()
     {
-        
+        // horizontal movement
+        float xSpeed = Input.GetAxis("Horizontal") * speed_const;
+        _rb.velocity = new Vector2(xSpeed, _rb.velocity.y);
+
+        // jumping
+        if(grounded && Input.GetButtonDown("Jump")){
+            _rb.AddForce(new Vector2(0, jump_const));
+        }
     }
 }
