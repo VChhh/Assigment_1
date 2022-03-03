2328a64baa8ad679beefa1a9aeccc853c22ee532
DrPeachy <p1067838263@gmail.com>
2022-03-02 19:00:53 -0500

    playerStats & publicVars

---
+++ Assets/codes/PlayerStats.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class PlayerStats : MonoBehaviour
+{
+
+    public TextMeshProUGUI playStats;
+
+
+    public LayerMask killable; // What layers can be grabbed
+
+    private float raycastDist = 3;
+
+    void Start()
+    {
+        display();
+    }
+
+    void display(){
+        playStats.text = "Level " + PublicVars.cur_level
+                        + "\nCoins: " + PublicVars.coins 
+                        + "\nScores: " + PublicVars.scores;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("collectiables")){
+            PublicVars.coins ++;
+            PublicVars.scores += 100;
+            Destroy(other.gameObject);
+            display();
+        }
+        else if(other.CompareTag("hidden")){
+            PublicVars.coins -= 50;
+            PublicVars.scores += 150;
+            display();
+        }
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/codes/PublicVars.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PublicVars
+{
+    public static int coins = 0;
+
+    public static int scores = 0;
+
+    public static int hiddens = 0;
+
+    public static int cur_level = 1;
+
+}
