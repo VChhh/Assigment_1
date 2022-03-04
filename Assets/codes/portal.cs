0ce2861d3908c489fe5d62bc1b1a65a035acfa75
VChhh <ql2015@nyu.edu>
2022-03-04 04:40:03 -0500

    portal_gun and aiming

--- Assets/codes/portal.cs
+++ Assets/codes/portal.cs
@@ -7,17 +7,23 @@ public class portal : MonoBehaviour
     public bool is_orange;
     public float teleport_dist = 0.2f;
     Transform dst;
+    public bool able_to_teleport = false;
     private void Start() {
-        if(is_orange){
-            dst = GameObject.FindGameObjectWithTag("blue_portal").GetComponent<Transform>();
-        } else {
-            dst = GameObject.FindGameObjectWithTag("orange_portal").GetComponent<Transform>();
-        }
+
+        //get_dst();
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(Vector2.Distance(transform.position, other.transform.position) > teleport_dist){
             other.transform.position = new Vector2(dst.position.x, dst.position.y);
+        }
+    }
+
+    public void get_dst(){
+        if(is_orange){
+            dst = GameObject.FindGameObjectWithTag("blue_portal").GetComponent<Transform>();
+        } else {
+            dst = GameObject.FindGameObjectWithTag("orange_portal").GetComponent<Transform>();
         }
     }
 
