c2d6d913e17bb10f7f77348c1d5b6f9a075ba297
DrPeachy <p1067838263@gmail.com>
2022-03-04 05:06:46 -0500

    grab

--- Assets/codes/level_entre.cs
+++ Assets/codes/level_entre.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class level_entre : MonoBehaviour
 {
+    public string next_level;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            PublicVars.cur_level++;
+            SceneManager.LoadScene(next_level);
+        }
     }
 }
