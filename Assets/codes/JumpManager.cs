1b9ba58002677bdb9ddfb2d184a7b33e8b968e55
Yeseon02 <yk2384@nyu.edu>
2022-03-11 17:58:57 +0400

    Jump Sound Added

---
+++ Assets/codes/JumpManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{   
+    //public static AudioClip playerShoot, playerJump; 
+    private AudioSource audioSrc;
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.Space)){
+            audioSrc.Play();
+        }
+    }
+}
