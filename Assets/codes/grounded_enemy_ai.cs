931a64090497a5d4d87950c50e19030c625f49a1
DrPeachy <p1067838263@gmail.com>
2022-03-10 23:37:34 -0500

    spider

--- Assets/codes/grounded_enemy_ai.cs
+++ Assets/codes/grounded_enemy_ai.cs
@@ -6,6 +6,8 @@ public class grounded_enemy_ai : MonoBehaviour
 {
     Transform player;
     Rigidbody2D _rb;
+
+    public Transform feet;
     public int chasing_dist = 10;
     public int speed_const = 12;
     public int jump_const = 100;
@@ -21,24 +23,24 @@ public class grounded_enemy_ai : MonoBehaviour
         StartCoroutine(moveloop());
     }
     private void FixedUpdate() {
-        Vector2 temp = new Vector2(transform.position.x, transform.position.y - 0.4f);
+        Vector2 temp = new Vector2(transform.position.x, feet.position.y);
         grounded = Physics2D.OverlapCircle(temp, ground_check_dist, ground);
     }
     private void Update() {
-        if( (player.position.x > transform.position.x && transform.localScale.x < 0) || 
-            (player.position.x < transform.position.x && transform.localScale.x > 0)) {
+        if( (player.position.x > transform.position.x && transform.localScale.x > 0) || 
+            (player.position.x < transform.position.x && transform.localScale.x < 0)) {
                 transform.localScale *= new Vector2 (-1, 1);
         }
     }
     IEnumerator moveloop(){
         while(true){
-            yield return new WaitForSeconds(Random.Range(0.3f, 0.5f));
+            yield return new WaitForSeconds(Random.Range(0.4f, 0.6f));
             
             if(Vector2.Distance(transform.position, player.position) < chasing_dist) {
                 if(grounded){
                     _rb.AddForce(new Vector2(0 , jump_const));
                 }
-                _rb.AddForce(new Vector2(transform.localScale.x * speed_const, 0));
+                _rb.AddForce(new Vector2(-transform.localScale.x * speed_const, 0));
             }
         } 
     }
