17ec74d607848e868cc4b50a286edd6471b293c4
Yeseon02 <yk2384@nyu.edu>
2022-03-11 23:19:47 +0400

    code clean

--- Assets/codes/MagicManager.cs
+++ Assets/codes/MagicManager.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class MagicManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     private AudioSource audioSrc;
     void Start()
     {
         audioSrc = GetComponent<AudioSource> ();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Fire1")){
